8a1f837878b956e65b7b490eb3d5b6c0b2e103d3
Jorge Vergara <javebratt@gmail.com>
2017-07-02 08:38:11 -0500

    Refactor code to match ES6 template strings

--- src/pages/event-detail/event-detail.ts
+++ src/pages/event-detail/event-detail.ts
@@ -19,37 +19,37 @@ export class EventDetailPage {
   constructor(public navCtrl: NavController, public navParams: NavParams, 
     public eventProvider: EventProvider, public cameraPlugin: Camera) {}
 
-    ionViewDidEnter(){
-      this.eventProvider.getEventDetail(this.navParams.get('eventId'))
-      .on('value', eventSnapshot => {
-        this.currentEvent = eventSnapshot.val();
-        this.currentEvent.id = eventSnapshot.key;
-      });
-    }
+  ionViewDidEnter(){
+    this.eventProvider.getEventDetail(this.navParams.get('eventId'))
+    .on('value', eventSnapshot => {
+      this.currentEvent = eventSnapshot.val();
+      this.currentEvent.id = eventSnapshot.key;
+    });
+  }
 
-    addGuest(guestName) {
-      this.eventProvider.addGuest(guestName, this.currentEvent.id, this.currentEvent.price, 
-      this.guestPicture).then(() => {
-        this.guestName = '';
-        this.guestPicture = null;
-      });
-    }
+  addGuest(guestName) {
+    this.eventProvider.addGuest(guestName, this.currentEvent.id, this.currentEvent.price, 
+    this.guestPicture).then(() => {
+      this.guestName = '';
+      this.guestPicture = null;
+    });
+  }
 
-    takePicture(){
-      this.cameraPlugin.getPicture({
-        quality : 95,
-        destinationType : this.cameraPlugin.DestinationType.DATA_URL,
-        sourceType : this.cameraPlugin.PictureSourceType.CAMERA,
-        allowEdit : true,
-        encodingType: this.cameraPlugin.EncodingType.PNG,
-        targetWidth: 500,
-        targetHeight: 500,
-        saveToPhotoAlbum: true
-      }).then(imageData => {
-        this.guestPicture = imageData;
-      }, error => {
-        console.log("ERROR -> " + JSON.stringify(error));
-      });
-    }
+  takePicture(){
+    this.cameraPlugin.getPicture({
+      quality : 95,
+      destinationType : this.cameraPlugin.DestinationType.DATA_URL,
+      sourceType : this.cameraPlugin.PictureSourceType.CAMERA,
+      allowEdit : true,
+      encodingType: this.cameraPlugin.EncodingType.PNG,
+      targetWidth: 500,
+      targetHeight: 500,
+      saveToPhotoAlbum: true
+    }).then(imageData => {
+      this.guestPicture = imageData;
+    }, error => {
+      console.log("ERROR -> " + JSON.stringify(error));
+    });
+  }
 
 }
